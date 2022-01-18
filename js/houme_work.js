e9207b03d935e2886fc554ce25c39d8d0d98f2f1
Selim Fazylov <selim.fazylov1@gmail.com>
2022-01-19 01:11:40 +0200

    home work

---
+++ js/houme_work.js
@@ -0,0 +1,16 @@
+const start = 5;
+const end = 10;
+const number = 7;
+
+
+function isNumberInRange(start, end, number) {
+  const isInRange = number;
+  if(number >= start && number <= end) {
+    const isInRange = number;
+  }
+   // Change this line
+
+  return isInRange;
+}
+
+console.log(isInRange);

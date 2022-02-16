5f71549e8ac6196b8f1cc7f311b02495376f3628
Selim Fazylov <selim.fazylov1@gmail.com>
2022-02-17 01:03:57 +0200

    timer

--- indexx.js
+++ indexx.js
@@ -3,4 +3,14 @@
 // console.log(validator.isEmail('foo@bar.com')); //=> true
 console.log("hi");
 
-import './js/modules.js';
+// import './js/modules.js';
+
+const greet = () => {
+  console.log("Hello!");
+};
+
+const timerId = setInterval(greet, 1000);
+
+console.log(timerId);
+// clearTimeout(timerId);
+clearInterval(timerId);

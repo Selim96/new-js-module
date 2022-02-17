61f9ababc78cd133c4b6c33c1b5ec16f54832201
Selim Fazylov <selim.fazylov1@gmail.com>
2022-02-17 20:45:05 +0200

    promises

--- indexx.js
+++ indexx.js
@@ -14,3 +14,40 @@ const timerId = setInterval(greet, 1000);
 console.log(timerId);
 // clearTimeout(timerId);
 clearInterval(timerId);
+// -------------------------------------
+// const isSuccess = true;
+
+// const promise = new Promise((resolve, reject) => {
+  
+//     if (isSuccess) {
+//       resolve("Success! Value passed to resolve function");
+//     } else {
+//       reject("Error! Error passed to reject function");
+//     }
+  
+// });
+
+// console.log("Before ");
+
+// promise.then(value => {
+//   console.log(value);
+// },
+//   error => {
+//     console.log(error);
+//   }
+// ).finally(() => console.log("Promise settled"));
+
+// console.log("After!");
+
+const makePromise = (text, delay) => {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(text), delay);
+  });
+};
+
+const promiseA = makePromise("promiseA value", 1000);
+const promiseB = makePromise("promiseB value", 3000);
+
+Promise.all([promiseA, promiseB])
+  .then(value => console.log(value)) //["promiseA value", "promiseB value"]
+  .catch(error => console.log(error));
